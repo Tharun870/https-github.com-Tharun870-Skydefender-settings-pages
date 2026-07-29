778b022988765ff936b9ff88ea1df9e087e37c3b
Tharun870 <tharunvaishnav56@gmail.com>
2026-07-29 19:09:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// SkyDefender JavaScript
+
+console.log("SkyDefender Loaded Successfully!");
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    alert("Welcome to SkyDefender! More features coming soon.");
+});
